477d0293c8c9d6661fe65c079962eeaa437a5851
Terrence Atkinson Jr <tatkinsonjr2@hotmail.com>
2023-06-19 20:54:01 -0400

    created html for create character.

--- web/src/pages/viewProfile.js
+++ web/src/pages/viewProfile.js
@@ -7,23 +7,24 @@ import * as url from "url";
 class ViewProfile extends BindingClass {
     constructor() {
         super();
-        this.bindClassMethods(['mount', 'getProfileForPage', 'getCharactersForPage', 'getRaidsForPage'], this);
+        this.bindClassMethods(['mount', 'getProfileForPage', 'getCharactersForPage', 'getRaidsForPage', 'createProfileTable'], this);
         this.dataStore = new DataStore();
         this.header = new Header(this.dataStore);
-        this.dataStore.addChangeListener(this.addProfileToPage);
+
         // this.dataStore.addChangeListener(this.addCharactersToPage);
         // this.dataStore.addChangeListener(this.addRaidsToPage);
     }
 
-    async updateProfile(event){
+    async updateProfile(event) {
         event.preventDefault();
         const urlParams = new URLSearchParams(window.location.search);
         const username = urlParams.get('username');
         const guild = document.getElementById('guild').value;
         const publicNote = document.getElementById('publicNote').value;
         const officerNote = document.getElementById('officerNote').value;
-        const profile = await this.client.updateProfile(username,guild,publicNote,officerNote);
+        const profile = await this.client.updateProfile(username, guild, publicNote, officerNote);
         this.dataStore.set('profile', profile);
+        console.log(profile);
         alert(username + "has been updated");
     }
 
@@ -51,86 +52,27 @@ class ViewProfile extends BindingClass {
         console.log('raids are stored');
     }
 
-    mount() {
+    async mount() {
         this.client = new SanctuaryRaiderClient();
-        this.getRaidsForPage();
-        this.getProfileForPage();
-        this.getCharactersForPage();
+        await this.getProfileForPage();
+        console.log('this is', this.dataStore.get('profile'));
+        await this.createProfileTable(this.dataStore.get('profile'));
+        // this.getRaidsForPage();
+        // this.getCharactersForPage();
     }
 
-    createProfileTable(profile) {
-        if (profile.length === 0) {
-            return '<h4>No profile found<h4>';
-        }
-        let html =
-            `<form>
-            <table class="table">
-                <thead class="thead-dark">
-                <tr>
-                    <th scope="col">Username</th>
-                    <th scope="col">Guild</th>
-                    <th scope="col">Public Note</th>
-                    <th scope="col">Officer Note</th>
-                </tr>
-                </thead>`
-        html += `
-    <tr>
-        <td>
-            <input readonly type="text" value="${profile.username}" id="username">
-        </td>
-        <td>
-            <input readonly type="text" value="${profile.guild}" id="guild">
-        </td>
-        <td>
-            <input readonly type="text" value="${profile.publicNote}" id="publicNote">
-        </td>
-        <td>
-            <input readonly type="text" value="${profile.officerNote}" id="officerNote">
-        </td>
-        <td>
-            <a href="editProfile.html?username=${profile.username}">
-                <span class="fas fa-fw fa-pencil">edit</span>
-            </a>
-        </td>
-    </tr> 
-   </table>
-   <span>
-     <button type="button" class="btn btn-success">Save</button> 
-     <i class="fa-thin fa-floppy-disk"></i> 
-     </span>
-</form>`;
-        return html;
+    async createProfileTable(profile) {
+        const profileName = document.getElementById("profile-username");
+        profileName.innerText = profile.username;
+        const guildName = document.getElementById("guild-name");
+        guildName.innerText = profile.guild;
+        console.log("html error");
     }
-    //
 
-    // createCharacterTable(characters) {
-    //     let html = `
-    //     <form>
-    //         <table class="table">
-    //             <thead class="thead-dark">
-    //             <tr>
-    //                 <th scope="col">Username</th>
-    //                 <th scope="col">Guild</th>
-    //                 <th scope="col">Public Note</th>
-    //                 <th scope="col">Officer Note</th>
-    //              </tr>
-    //             </thead>
-    //     `
-    // }
-
-    addProfileToPage() {
-        const profile = this.dataStore.get('profile');
-        if (profile == null) {
-            return;
-        }
-        document.getElementById('viewProfileTable').innerHTML = this.createProfileTable(profile);
-        // document.getElementById('saveProfile').addEventListener('click', this.updateProject);
-    }
 }
-
 const main = async() => {
     const viewProfile = new ViewProfile();
-    viewProfile.mount()
+    await viewProfile.mount();
 };
  window.addEventListener('DOMContentLoaded', main);
 
